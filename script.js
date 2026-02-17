826d60042fb502c1b982f5ef0a5ea155dac1565d
ErickJosafatEstradaGutierrez <role523124@gmail.com>
2026-02-17 15:57:48 -0600

    Segundaa version landing

--- script.js
+++ script.js
@@ -9,5 +9,46 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 60 },
+    color: { value: "#38bdf8" },
+    size: { value: 3 },
+    move: { speed: 1 },
+    line_linked: { enable: true }
+  }
+});
+
+const ctx = document.getElementById('myChart');
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['CCTV', 'Inventario', 'Cotizaciones', 'Automatización'],
+    datasets: [{
+      data: [85, 70, 90, 95],
+    }]
+  },
+});
+
+function openModal() {
+  document.getElementById("modal").style.display = "block";
+}
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
+function toggleTheme() {
+  document.body.classList.toggle("light");
+}
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach(sec => {
+    const top = sec.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      sec.classList.add("show");
+    }
+  });
+});
+
 
 window.onload = typeWriter;
